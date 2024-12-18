7eed7c770adb023f887430a7a7a687f7d274ea15
bppwilde <pwilde@bennett-pless.com>
2024-12-18 17:04:35 -0500

    test

--- Tilt_Panel_Rebar_Streamlit.py
+++ Tilt_Panel_Rebar_Streamlit.py
@@ -28,7 +28,7 @@ if uploaded_files:
     
     st.success(f"Files uploaded successfully to {temp_dir}")
 
-    st.write(uploaded_files.count())
+    st.write(f"Number of files: {uploaded_files.count()}")
 #%%
 
 """

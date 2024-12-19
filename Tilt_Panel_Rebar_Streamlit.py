7d8dee6d65dcb494e399ca91d3a0de8fec7a22a1
bppwilde <pwilde@bennett-pless.com>
2024-12-19 09:45:29 -0500

    read files initial

--- Tilt_Panel_Rebar_Streamlit.py
+++ Tilt_Panel_Rebar_Streamlit.py
@@ -16,31 +16,71 @@ def test_plot(x, s_title):
     ax.set_title("Simple Line Plot")
 
     st.pyplot(fig)
+
 #%% File uploader, that accepts only tup files. Other files can be uploaded but only the tup files will be accepted.
 accepted_ftype = ['tup']
 st.title("Folder Upload System")
 
 uploaded_files = st.file_uploader("Choose files from a folder", accept_multiple_files=True, type=accepted_ftype)
 
+# Define a list of items to look for in each line
+items = ['PanelType', 'ParapetHeight', 'BottomPanelHeight', 'PanelHeight', 'PanelLength', 'PanelThickness', 'PanelMaterial', 'Openings', 'DataVBarsCount', 'DataVBarsVBars','DataHBarsCount', 'DataHBarsHBars']
+
+# Initialize an empty dictionary for each item
+item_dict = {key: np.nan for key in items}
+
+# Define a blank Dataframe with the column names
+df = pd.DataFrame(columns = items)
+
 if uploaded_files:
-    st.write("Uploaded files:")
-    for file in uploaded_files:
-        st.write(f"- {file.name}")
-    
     # Create a temporary directory to store uploaded files
     temp_dir = "temp_wall_files"
     os.makedirs(temp_dir, exist_ok=True)
     i = 0
     for file in uploaded_files:
-        first_line = file.readline().decode('utf-8').strip()
-        i+=1
-        test_plot(i,first_line)
+        if file.endswith('.tup'):
+            # Set Panel type to the name of the .tup file
+            item_dict['PanelType']=file.replace('.tup', '')
 
-        file_path = os.path.join(temp_dir, file.name)
-        with open(file_path, "wb") as f:
-            f.write(file.getbuffer())
+            # Read in the file
+            with open(folder + "\\" + file, 'r') as f:
+                lines = f.readlines()
+            # Loop through each line in the file
+            for line in lines:
+                # Loop through each item in the items list
+                for item in items:
+                    # Check if the item is in the line
+                    if line.startswith(f"{item}"):
+                        try:
+                            # Extract the value from the line
+                            value = str(line.split("=")[1].strip()).replace(" ft", "").replace(" in", "")
+                            if value.startswith('C'):
+                                value = value.split("-")[0].strip().replace("C ", "")
+                            # Append the value to the dictionary with a semicolon separator
+                            if str(item_dict[item]) == 'nan':
+                                item_dict[item] = value
+                            else:
+                                item_dict[item] = str(item_dict[item]) + ';' + value
+                            break
+                        except:
+                            # If the item is not in the line, append None to the dictionary
+                            item_dict[item] = None
+
+            df1 = pd.DataFrame.from_dict([item_dict])
+            df = pd.concat([df, df1], ignore_index=True)
+            for key in item_dict:
+                item_dict[key] = np.nan
+
+        # Change Material to psi and thickness add inches
+        df['PanelMaterial'] = df['PanelMaterial'] + '000 psi'
+        df['PanelThickness'] = df['PanelThickness'] + ' inches'
+        df['Tfc'] = 'T=' + df['PanelThickness'] + '/f\'c=' + df['PanelMaterial']
+    
     
     st.success(f"Files uploaded successfully to {temp_dir} folder.")
 
     st.write(f"Number of files: {len(uploaded_files)}")
+    
+    #Display the dataframe from reading the tup files for testing purposes
+    st.dataframe(df)
 

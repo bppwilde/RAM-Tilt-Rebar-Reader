14c9c66ab22bd65b5757196f0c7f763184b40170
bppwilde <pwilde@bennett-pless.com>
2024-12-19 08:03:12 -0500

    Testing multi plots

--- Tilt_Panel_Rebar_Streamlit.py
+++ Tilt_Panel_Rebar_Streamlit.py
@@ -5,7 +5,17 @@ import pandas as pd
 import streamlit as st
 import os
 import matplotlib.pyplot as plt
+#%% Test Plot
+def test_plot(x, s_title):
+    st.title(f"Matplotlib Plot #{x}")
+    st.header(f"Line = {s_title}")
+    fig, ax = plt.subplots()
+    ax.plot([1, 2, 3, 4, 5])
+    ax.set_xlabel("X-axis")
+    ax.set_ylabel("Y-axis")
+    ax.set_title("Simple Line Plot")
 
+    st.pyplot(fig)
 #%% File uploader, that accepts only tup files. Other files can be uploaded but only the tup files will be accepted.
 accepted_ftype = ['tup']
 st.title("Folder Upload System")
@@ -20,12 +30,11 @@ if uploaded_files:
     # Create a temporary directory to store uploaded files
     temp_dir = "temp_wall_files"
     os.makedirs(temp_dir, exist_ok=True)
-    
+    i = 0
     for file in uploaded_files:
         first_line = file.readline().decode('utf-8').strip()
-        st.write(f"First line of {file.name}: {first_line}")
-        first_line = file.readline().decode('utf-8').strip()
-        st.write(f"First line of {file.name}: {first_line}")
+        i+=1
+        test_plot(i,first_line)
 
         file_path = os.path.join(temp_dir, file.name)
         with open(file_path, "wb") as f:
@@ -34,13 +43,4 @@ if uploaded_files:
     st.success(f"Files uploaded successfully to {temp_dir} folder.")
 
     st.write(f"Number of files: {len(uploaded_files)}")
-#%% Test Plot
-st.title("Matplotlib Plot in Streamlit")
 
-fig, ax = plt.subplots()
-ax.plot([1, 2, 3, 4, 5])
-ax.set_xlabel("X-axis")
-ax.set_ylabel("Y-axis")
-ax.set_title("Simple Line Plot")
-
-st.pyplot(fig)
